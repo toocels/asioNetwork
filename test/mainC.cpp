b81ba9694e0b49bda6f89a9b5b0463f09d876be4
Toocels <toocels@gmail.com>
2022-09-04 19:24:35 +0530

    Added basic test program

--- test/mainC.cpp
+++ test/mainC.cpp
@@ -1,17 +1,17 @@
-// #include <iostream>
-// #include <string>
-// #include <asio.hpp>
+#include <iostream>
 
 #include "Client.h"
 
+using std::cout, std::endl;
+
 int main() {
-	// std::cout << "ytes" << std::endl;
+	
+	Client client;
+	if (!client.connect(69, "127.0.0.1"))
+		return 0; // failed
 
-	// Client a;
-	// if (!a.connect(69, "127.0.0.1"))
-	// 	return 0; // failed
+	client.sendMsg("Msg from cleint :)");
+	std::cout << client.fetch_msg() << std::endl;
 
-	// sleep(1);
-	// std::cout << a.fetch_msg() << std::endl;
 	return 0;
 }
--- test/mainS.cpp
+++ test/mainS.cpp
@@ -1,13 +1,22 @@
-// #include <iostream>
-// #include <string>
-// #include <asio.hpp>
+#include <iostream>
+#include <thread>
 
 #include "Server.h"
 
+using std::cout, std::endl;
+
+void recvMsg(std::string msg, Server *server){
+	cout << "Msg from client: " << msg << endl;
+}
+
 int main() {
 
-	// auto a = new Client();
-	// Server a(69, "127.0.0.1");
-	// a.listen_connections();
+	Server server(69, "127.0.0.1", "./out/log.txt");
+	
+	cout << ":Listening for connections:" << endl;
+	std::thread thread_obj(&Server::listen_connections, &server);
+
+	server.listen_messages(recvMsg);
+
 	return 0;
 }
