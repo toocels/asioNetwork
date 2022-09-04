f18c23e0a0041a6c2aef1e5bd5afa5ad1f3f3bc6
Toocels <toocels@gmail.com>
2022-09-04 19:12:55 +0530

    Instaling lib with messages

--- installLib.sh
+++ installLib.sh
@@ -1,8 +1,14 @@
+echo "Installing custom library asioNetwork"
+
 gcc -c src/server.cpp -Iinclude -o obj/server.o
 gcc -c src/client.cpp -Iinclude -o obj/client.o
+echo "Done compiling .cpp"
 
 ar cr /usr/lib/cstm_libs/libserver.a obj/server.o
 ar cr /usr/lib/cstm_libs/libclient.a obj/client.o
+echo "Done creating object files."
 
 cp include/Client.h /usr/include/cstm_libs/Client.h
 cp include/Server.h /usr/include/cstm_libs/Server.h
+echo "Done copying header files."
+echo ""
